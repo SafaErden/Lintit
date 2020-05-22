1e692f5bde01b548c61ab8c45d679d751d27cfc2
Safa ERDEN <safaerden@gmail.com>
2020-05-22 19:15:27 +0300

    fixed sample

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 gem 'colorize', '~> 0.8.1'
-gem 'parser', '2.4.0.2'
 gem 'rubocop'
--- sample.rb
+++ sample.rb
@@ -1,91 +1,34 @@
-
-def bubble_sort(    arr)
-
-           arr.each           do
-                          i          = 0;
-
-
-    while i<arr.length-1
-           arr[ i], arr[i + 1] =   arr[ i + 1],    arr[i ] if arr[i] > arr[i + 1]
-      i += 1;
-
+  
+def bubble_sort(arr)
+  arr.each do
+    i = 0
+    while i < arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+      i += 1
     end
   end
   arr
-
-
-
-
-
-
-
 end
 
-
-
-
-
-
-
-
-
-
-
-
-def bubble_sort_by             (arr)
-a= { a:"a"}
+def bubble_sort_by(arr)
   arr.each do
-    i = 0;
-    while i <=arr.length - 1
-           arr[i], arr[i + 1 ] =             arr[ i + 1], arr[i] if          yield(arr[i], arr[i + 1]). positive?
-      i +=                             1;
-      end
+    i = 0
+    while i < arr.length - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]). positive?
+      i += 1
+    end
   end
   arr
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def sortit
-
-  def another_def
-  end
+bubble_by = bubble_sort_by([4, 3, 78, 2, 0, 2, -2, -5]) do |x, y|
+  x - y
 end
 
-
-
-   bubble_by            = bubble_sort_by
-     x-y;
- arr=[]
- arr.map!   !
-
-end
-
-
-bubble_by_test = bubble_sort_by( %w[hi hello hey ]   ) {|left, right| left.length - right.length }
+bubble_by_test = bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
 
 print bubble_sort(%w[orange tomato apple strawberry coconut])
-        puts
+puts
 print bubble_by
-     puts
+puts
 print bubble_by_test
-
-class TestCase
-end
-
-module Csed
-end
-
